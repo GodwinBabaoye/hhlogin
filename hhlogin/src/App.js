233bf0bb6963d75bb24271d783042f4697aa84fe
GodwinBabaoye <Godwin.Babaoye@cdpipelinedevshop.com>
2022-07-20 14:17:49 +0000

    first commit

---
+++ hhlogin/src/App.js
@@ -0,0 +1,15 @@
+
+
+import logo from './logo.svg';
+import './App.css';
+import LogIn from './components/LogIn'
+
+function App() {
+  return (
+    <div>
+      <LogIn></LogIn>
+    </div>
+  );
+}
+
+export default App;
